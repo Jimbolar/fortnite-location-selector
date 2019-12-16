01ca1b8b0e542c9594b9350318a34ece17182518
Jimbolar <james.robinson@sky.uk>
2019-12-16 23:36:42 +0000

    further changes to index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.use('/client', express.static(path.join(__dirname + '/client')));
 
-// express knows to serve the index.html from within a directory ^
+// express knows to serve the index.html from within a directory. The directory in this case is ../client.
 
 app.get("/", (req, res) =>  {
     res.sendFile(path.join(__dirname + "/client/index.html"))
@@ -18,18 +18,19 @@ app.get("/butterpants", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("Hello world");
-});
-
-app.use((req, res) => {
-res.status(404)
-  .send('Unknown Request');
+  res.send("Hello World");
 });
 
 app.get("*", (req, res) => {
-    console.log(req.path);
+    console.log('error', req.path);
+    res.status(404);
     res.json({ error: "you do not know de way" });
 });
+
+// app.use((req, res) => {
+// res.status(404)
+//   .send('Unknown Request');
+// });
 
 app.listen(PORT, function(){
     console.log(`Server is listening on port ${PORT}`);
